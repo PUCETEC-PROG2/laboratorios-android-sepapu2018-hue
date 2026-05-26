3031c0d613c329e10caf4fa17c694c47d1fdb3a7
Narvaez Jose <sepapu2018@gmail.com>
2026-05-26 18:16:26 -0500

    Laboratotio 2 Desarrolo de interfaz  creacion de models acatulizacion Jose Narvaez

--- app/src/main/java/ec/edu/puce/githubclient/models/services/ApiService.kt
+++ app/src/main/java/ec/edu/puce/githubclient/models/services/ApiService.kt
@@ -2,8 +2,15 @@ package ec.edu.puce.githubclient.models.services
 
 import ec.edu.puce.githubclient.models.Repository
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET( value = "/user/repos")
-    suspend fun getRepositories () : List<Repository>
+    suspend fun getRepositories (
+        @Query( value = "sport") sort: String = "created",
+        @Query( value = "order") order: String = "desc",
+        @Query( value = "affiliation") affiliation: String = "owner",
+        @Query( value = "per_page") perPage: Int = 100,
+        @Query( value = "t") t: String = "${System.currentTimeMillis()}",
+    ) : List<Repository>
 }
